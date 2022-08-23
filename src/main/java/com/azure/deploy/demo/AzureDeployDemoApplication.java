c3dde0cf44ff60db5a07211eb02ba6c098e66bcd
smasanta <107704230+smasanta@users.noreply.github.com>
2022-08-23 16:06:44 +0530

    first commit

---
+++ src/main/java/com/azure/deploy/demo/AzureDeployDemoApplication.java
@@ -0,0 +1,20 @@
+package com.azure.deploy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureDeployDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats Saby ! you app deployed successfully in Azure !";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDeployDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/deploy/demo/AzureDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.deploy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
